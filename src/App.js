2bac7467e79ff9e02399af3a4006b5d95af1bf36
Vanessa Bautista <vanessawebdev@gmail.com>
2022-10-14 11:38:08 -0700

    added useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+let born = false;
+
 function App() {
+  const [growth, setGrowth] = useState(0);
+  const [nirvana, setNirvana] = useState(false);
+
+  // Runs on first render
+  useEffect(()=>{
+    console.log("I am born")
+  }, []);
+
+  // Runs on initial mount and every update
+  useEffect(()=>{
+    console.log("make mistake and learn")
+
+    if(growth > 70){
+      setNirvana(true);
+    }
+    return function cleanup(){
+      console.log("cleanup after mistakes");
+    }
+  })
+
+  // Does not run on initial mount but DOES run on every update
+  useEffect(()=>{
+    if(born){
+      console.log("I am alive")
+    }else{
+      born = true
+    }
+  })
+
+  //Runs when Nirvana changes
+  useEffect(()=>{
+    if(born){
+      document.title = "Nirvana attained"
+    }
+  }, [nirvana])
+
+  const growHandle =() =>{
+    setGrowth(growth + 10)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Use Effect</h2>
+        <h3>growth: {growth}</h3>
+        <button onClick={growHandle}>Learn and grown</button>
       </header>
     </div>
   );
