5026f0fae6cc7c47207800ae6185c130f169c15b
Vanessa Bautista <vanessawebdev@gmail.com>
2022-11-29 13:46:26 -0800

    added custom fetch hook

---
+++ src/App-Versions/App-useLayoutEffect.js
@@ -0,0 +1,37 @@
+import React, { useState, useRef, useLayoutEffect } from "react";
+
+import "../App.css";
+
+function useDim(el, val) {
+  const [height, setHeight] = useState(0);
+  const [width, setWidth] = useState(0);
+  useLayoutEffect(() => {
+    let boundingBox = el.current.getBoundingClientRect();
+    setHeight(boundingBox.height);
+    setWidth(boundingBox.width);
+  }, [val]);
+  return { height, width };
+}
+
+function App() {
+  const [val, setVal] = useState(2);
+  const valEl = useRef(null);
+
+  const { height, width } = useDim(valEl, val);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          {" "}
+          Height is {height}, width is {width}
+        </h1>
+        <h2>useLayoutEffect</h2>
+        <div ref={valEl}>{val}</div>
+        <button onClick={() => setVal(val * 10)}>10 *</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/hooks/useCustomFetch.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+function useCustomFetch(url) {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  async function customFetch(url) {
+    try {
+      let response = await fetch(url);
+      let rData = await response.json();
+
+      setData({ rData });
+      setLoading(false);
+    } catch (e) {
+      setError(e);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      if (url) {
+        customFetch(url);
+      }
+    }, 3000);
+  }, [url]);
+
+  return [data, loading, error];
+}
+
+export default useCustomFetch;
