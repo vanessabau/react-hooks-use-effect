d9e2a80dcbcd3e1ddce03c098ba33a2e022d3bf4
Vanessa Bautista <vanessawebdev@gmail.com>
2022-11-30 09:53:26 -0800

    added Navlink and Navigate

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   BrowserRouter,
   Routes,
   Route,
   NavLink,
   useParams,
+  Navigate,
 } from "react-router-dom";
 //import Navbar from "./navbar/index";
 import HomePage from "./pages/HomePage";
@@ -23,25 +24,71 @@ function Child() {
 }
 
 function AppRouter() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  // This styling will be applied to a <NavLink> when the
+  // route that it links to is currently selected.
+  let activeStyle = {
+    textDecoration: "underline",
+    color: "lavenderblush",
+  };
+
+  let activeClassName = "underline";
+
+  const onClickHandle = () => {
+    setLoggedIn(!loggedIn);
+  };
+
   return (
     <BrowserRouter>
       <div className="navbar">
         <ul id="horizontal-list">
           <li>
-            <NavLink to="/">Home</NavLink>
+            <NavLink
+              to="/"
+              exact="true"
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/about">About</NavLink>
+            <NavLink
+              to="/about"
+              exact="true"
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              About
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/users/:username">UserPage</NavLink>
+            <NavLink
+              to="/user/john/doe"
+              exact="true"
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              User John Doe
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/users/:username"
+              exact="true"
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              UserPage
+            </NavLink>
           </li>
         </ul>
       </div>
+      <h3>logged in: {loggedIn.toString()}</h3>
+      <button onClick={onClickHandle}>{loggedIn ? "logout" : "login"}</button>
       <Routes>
         <Route path="/" exact element={<HomePage />} />
         <Route path="/about" exact element={<AboutPage />} />
-        <Route path="/users/:username" element={<Child />} />
+        <Route
+          path="/users/:username"
+          element={loggedIn ? <Child /> : <Navigate to="/" />}
+        />
       </Routes>
     </BrowserRouter>
   );
